020e504b7169909189a7a8caf5b99d55c7ac2e3b
MarconiCalvo <marconi.calvo.campos@est.una.ac.cr>
2025-03-29 18:23:04 -0600

    [UPDATE] Changes in middleware

--- currency-converter/src/app/middleware.ts
+++ currency-converter/src/app/middleware.ts
@@ -2,8 +2,9 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req: Request) {
   const apiKey = process.env.CURRENCY_API_KEY;
+  const authHeader = req.headers.get("authorization");
 
-  if (!apiKey) {
+  if (!authHeader || authHeader !== `Bearer ${apiKey}`) {
     return NextResponse.redirect('/error?message=API%20key%20is%20missing');
   }
 
